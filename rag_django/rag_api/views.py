fd7ba7acfa1757a49bbffe7946592c1c3cf4d5ad
jal9o3 <119987573+jal9o3@users.noreply.github.com>
2024-06-17 19:33:12 +0800

    query encoding and decoding

--- rag_django/rag_api/views.py
+++ rag_django/rag_api/views.py
@@ -4,8 +4,11 @@ from rest_framework.decorators import api_view
 
 from django.http import JsonResponse
 
+from urllib.parse import unquote
+
 # Create your views here.
-#@api_view(['GET'])
+
 def query(request, query):
+    query = unquote(query)
     data = {"query": query}
     return JsonResponse(data)
