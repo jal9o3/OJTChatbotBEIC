7c5fd974a7c4c753c0420b38804cc173e3634290
jal9o3 <119987573+jal9o3@users.noreply.github.com>
2024-06-18 09:07:14 +0800

    added api_test.py for future reference

---
+++ api_test.py
@@ -0,0 +1,21 @@
+import requests
+from urllib.parse import quote
+
+def replace_forward_slashes(input_string):
+    return input_string.replace('/', ' ')
+
+query = "hello!:mfs// lmao"
+# query = "hello!:mfs lmao"
+# query = "hello there Everyone!"
+
+query = replace_forward_slashes(query)
+encoded_query = quote(query, safe='')
+print("Encoded Query: " + encoded_query)
+
+response = requests.get('http://127.0.0.1:8000/query/' + encoded_query)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f'Error: {response.status_code}')
