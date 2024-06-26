1c0ba760b721b65576ba7da4295aae705c900442
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2024-06-26 14:53:03 +0800

    file upload working in django rest framework's provided page

--- rag_django/rag_api/urls.py
+++ rag_django/rag_api/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 from django.conf import settings
 
+from .views import FileUploadAPIView
+
 urlpatterns = [
     path('query/<str:query>/<int:n_results>/', views.query, name='query'),
-    #path('post/', views.postData),
+    path('upload-file/', FileUploadAPIView.as_view(), name='upload_file')
 ]
--- rag_django/rag_api/views.py
+++ rag_django/rag_api/views.py
@@ -9,6 +9,13 @@ from urllib.parse import unquote
 import chromadb
 import mysql.connector
 
+from rest_framework import status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import FileUploadSerializer
+
 from ojt_modules import chroma_functions
 from ojt_modules import mysql_functions
 
@@ -25,3 +32,44 @@ def query(request, query, n_results):
     
     # data = {"query": query}
     return JsonResponse(results, safe=False)
+
+class FileUploadAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    serializer_class = FileUploadSerializer
+
+    def post(self, request, *args, **kwargs):
+        print("File Upload view opened.")
+        serializer = self.serializer_class(data=request.data)
+        print("Serializer:")
+        print(serializer)
+        print("Request:")
+        print(request)
+        print("Request Data:")
+        print(request.data)
+        print("Checking if valid...")
+        if serializer.is_valid():
+            # You can access the file like this from serializer:
+            # uploaded_file = serializer.validated_data["file"]
+            print("Saving...")
+            serializer.save()
+            print("Saved!")
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        
+        print("Invalid!")
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    # def post(self, request, format=None):
+    #     print(request.FILES.keys())
+    #     my_file = request.FILES['file_field_name']
+    #     filename = '/tmp/myfile'
+    #     with open(filename, 'wb+') as temp_file:
+    #         for chunk in my_file.chunks():
+    #             temp_file.write(chunk)
+
+    #     my_saved_file = open(filename) #there you go
