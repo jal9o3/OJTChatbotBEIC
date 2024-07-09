78ee170fa916c3cf99b28917a0bfc7de60fea259
LilNewton <ceejayborgz27@gmail.com>
2024-07-09 16:55:37 +0800

    added script to upload sample text data to database

--- upload.py
+++ upload.py
@@ -2,35 +2,108 @@ from demo_main import format_upload_doc
 
 format_upload_doc(
         filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Ashmore_Reef_1_WCR_Basic_Interp_I00025257.txt",
-        title='',
-        authors=['Australian '],
-        tags=['Science Fiction', '1800s', 'Alien']
+        title='Ashmore_Reef_1_WCR_Basic_Interp_I00025257',
+        authors=['Unknown'],
+        tags=['Unknown']
     )
 
 format_upload_doc(
-        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Ashmore_Reef_1_WCR_Basic_Interp_I00025257.txt",
-        title='War of the Worlds',
-        authors=['H.G. Wells'],
-        tags=['Science Fiction', '1800s', 'Alien']
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Barnett_3_WCR_Vol_1_Basic_Data_Part_1_I00029391.txt",
+        title='Barnett_3_WCR_Vol_1_Basic_Data_Part_1_I00029391',
+        authors=['Unknown'],
+        tags=['Unknown']
     )
 
 format_upload_doc(
-        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Ashmore_Reef_1_WCR_Basic_Interp_I00025257.txt",
-        title='War of the Worlds',
-        authors=['H.G. Wells'],
-        tags=['Science Fiction', '1800s', 'Alien']
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Calytrix_1_WCR_Basic_I00015811_Bonaparte_additional_Calytrix1.txt",
+        title='Calytrix_1_WCR_Basic_I00015811_Bonaparte_additional_Calytrix1',
+        authors=['Unknown'],
+        tags=['Unknown']
     )
 
 format_upload_doc(
-        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Ashmore_Reef_1_WCR_Basic_Interp_I00025257.txt",
-        title='War of the Worlds',
-        authors=['H.G. Wells'],
-        tags=['Science Fiction', '1800s', 'Alien']
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Challis_8_WCR_Interpretive_I00029770.txt",
+        title='Challis_8_WCR_Interpretive_I00029770',
+        authors=['Unknown'],
+        tags=['Unknown']
     )
 
 format_upload_doc(
-        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Ashmore_Reef_1_WCR_Basic_Interp_I00025257.txt",
-        title='War of the Worlds',
-        authors=['H.G. Wells'],
-        tags=['Science Fiction', '1800s', 'Alien']
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Sample Results\Challis_14_WCR_Interpretive_Data_I00011470.txt",
+        title='Challis_14_WCR_Interpretive_Data_I00011470',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+
+
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\A_Case_Study_of_Understanding_the_Bonaparte_Basin.txt",
+        title='A_Case_Study_of_Understanding_the_Bonaparte_Basin',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\An_Automated_Information_Retrieval_Platform_For_Unstructured_Well_Data.txt",
+        title='An_Automated_Information_Retrieval_Platform_For_Unstructured_Well_Data',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\CO2_emissions_the_elephant_in_the_room.txt",
+        title='CO2_emissions_the_elephant_in_the_room',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Double_funnel_approach_for_screening_of_potential_CO2_storage_opportunities.txt",
+        title='Double_funnel_approach_for_screening_of_potential_CO2_storage_opportunities',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Sand_Production_and_Control_Benchmarking.txt",
+        title='Sand_Production_and_Control_Benchmarking',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Scaling_and_optimizing_performance_and_cost_of_machine_learning_ingestion.txt",
+        title='Scaling_and_optimizing_performance_and_cost_of_machine_learning_ingestion',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Supporting_the_UN_2050_Net_Zero_goals_by_reading_the_earth_better.txt",
+        title='Supporting_the_UN_2050_Net_Zero_goals_by_reading_the_earth_better',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Sustainable_data_mining_AI_ML-based_parameter_extraction.txt",
+        title='Sustainable_data_mining_AI_ML-based_parameter_extraction',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Using_Machine_Learning-Based_Data_Factory_to_Unlock_Mining_in_Australia.txt",
+        title='Using_Machine_Learning-Based_Data_Factory_to_Unlock_Mining_in_Australia',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\Utilizing_Machine_Learning_to_Gain_Geological_Insights.txt",
+        title='Utilizing_Machine_Learning_to_Gain_Geological_Insights',
+        authors=['Unknown'],
+        tags=['Unknown']
     )
