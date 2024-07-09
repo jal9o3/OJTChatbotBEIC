bfd7a7ce727fa13c5024fb1034a42a9fc6eb1830
LilNewton <ceejayborgz27@gmail.com>
2024-07-09 16:56:34 +0800

    added more of the text

--- upload.py
+++ upload.py
@@ -35,8 +35,40 @@ format_upload_doc(
         tags=['Unknown']
     )
 
+format_upload_doc(
+        filename=r"Tesseract/Sample Results/Corallina_2_WCR_Vol_2_Appendices_I00033046.txt",
+        title='Corallina_2_WCR_Vol_2_Appendices_I00033046',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
 
+format_upload_doc(
+        filename=r"Tesseract/Sample Results/ENO362292_Laminaria_Pilot_South_1_WCR_Basic_Data.txt",
+        title='ENO362292_Laminaria_Pilot_South_1_WCR_Basic_Data',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
 
+format_upload_doc(
+        filename=r"Tesseract/Sample Results/ENO466618_Wisteria_1_Basic_WCR.txt",
+        title='ENO466618_Wisteria_1_Basic_WCR',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"Tesseract/Sample Results/Nancar_1_WCR_Basic_Data_I00005928_Bonaparte_additional_Nancar1.txt",
+        title='Nancar_1_WCR_Basic_Data_I00005928_Bonaparte_additional_Nancar1',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
+
+format_upload_doc(
+        filename=r"Tesseract/Sample Results/Petrel_3_WCR_Addendum_6_Well_Test_Interpretation_Reports_DST_1_2_3_I00026956.txt",
+        title='Petrel_3_WCR_Addendum_6_Well_Test_Interpretation_Reports_DST_1_2_3_I00026956',
+        authors=['Unknown'],
+        tags=['Unknown']
+    )
 
 format_upload_doc(
         filename=r"E:\Github\OJTChatbotBEIC\Tesseract\Technical Results\A_Case_Study_of_Understanding_the_Bonaparte_Basin.txt",
