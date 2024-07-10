93cd273455d98613ac1e8a6be303c7ed1929253e
jal9o3 <jeromeardiente.loria@bicol-u.edu.ph>
2024-07-10 11:16:01 +0800

    component for generating embedding using all-MiniLM-L6-v2 model passes test (had to remove problematic temp folder by Tensorflow)

--- test_vector_management.py
+++ test_vector_management.py
@@ -8,7 +8,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Set to '0', '1', or '2'
 import tensorflow_hub as hub
 
-from vector_management import get_USE_embedding
+from vector_management import get_USE_embedding, get_MiniLM_embedding
 
 class TestUSEEmbedding(unittest.TestCase):
     def test_embedding(self):
@@ -16,6 +16,13 @@ class TestUSEEmbedding(unittest.TestCase):
         # Test a sample sentence
         sentence = "This is a test sentence."
         embedding = get_USE_embedding(sentence)
+        self.assertIsNotNone(embedding)
+
+class TestMiniLMEmbedding(unittest.TestCase):
+    def test_embedding(self):
+        # Test case: Check if the embedding is not None
+        sentence = "This is a test sentence."
+        embedding = get_MiniLM_embedding(sentence)
         self.assertIsNotNone(embedding)
 
 if __name__ == "__main__":
--- vector_management.py
+++ vector_management.py
@@ -5,9 +5,19 @@ import os
 # Must be done before Tensorflow is imported as per this Stackoverflow answer:
 # https://stackoverflow.com/a/64448286
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Set to '0', '1', or '2'
+# TensorFlow Hub reads the model directly from remote storage
+# os.environ["TFHUB_MODEL_LOAD_FORMAT"] = "UNCOMPRESSED"
 import tensorflow_hub as hub
+
+from sentence_transformers import SentenceTransformer
 
 def get_USE_embedding(sentence):
     logging.getLogger('tensorflow').setLevel(logging.FATAL)
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
     return embed([sentence])[0]
+
+def get_MiniLM_embedding(sentence):
+    # Load a pretrained Sentence Transformer model (e.g., 'all-MiniLM-L6-v2')
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    embedding = model.encode([sentence])
+    return embedding[0]
