e40df44eb67ec087adf9e3ad60d1cf11858a11b2
LilNewton <ceejayborgz27@gmail.com>
2024-07-17 08:54:33 +0800

    Removed some unnecesssary files

--- Tesseract/preprocessing.py
+++ Tesseract/preprocessing.py
@@ -1,19 +1,8 @@
-import pytesseract
 from pdf2image import convert_from_path
-import os
+
 import cv2 as cv
 import numpy as np
-import pandas as pd
-from io import BytesIO
-from PIL import Image
-import time
 
-import mysql.connector
-from mysql.connector import errorcode
-
-import streamlit as st
-
-import os
 import string
 import re
 import nltk
